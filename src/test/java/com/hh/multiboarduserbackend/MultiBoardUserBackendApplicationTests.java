187655d63cf1ee68b2b4a5e9ae9ea10b21a28ee3
bboodd <gkrgus9@gmail.com>
2024-09-02 16:01:43 +0900

    Initial commit + README

---
+++ src/test/java/com/hh/multiboarduserbackend/MultiBoardUserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh.multiboarduserbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiBoardUserBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
