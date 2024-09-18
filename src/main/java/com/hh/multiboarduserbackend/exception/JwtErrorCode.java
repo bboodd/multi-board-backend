beb9e2c736eae3d2e668c41c28cc1c4e0b985b94
bboodd <gkrgus9@gmail.com>
2024-09-18 23:26:09 +0900

    자유게시판 글 등록  상세구현

--- src/main/java/com/hh/multiboarduserbackend/exception/JwtErrorCode.java
+++ src/main/java/com/hh/multiboarduserbackend/exception/JwtErrorCode.java
@@ -6,7 +6,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum JwtErrorCode implements ErrorCode {
 
-    REQUIRED_ACCESS_TOKEN("엑세스 토큰이 필요한 요청입니다.", HttpStatus.UNAUTHORIZED);
+    REQUIRED_ACCESS_TOKEN("엑세스 토큰이 필요한 요청입니다.", HttpStatus.UNAUTHORIZED),
+    RESOLVE_TOKEN_ERROR("토큰을 변환할 수 없습니다.", HttpStatus.UNAUTHORIZED),
+    NOT_AUTH_TOKEN_ERROR("AUTH TOKEN 이 아닙니다.", HttpStatus.UNAUTHORIZED),
+    WRONG_ACCESS_TOKEN("잘못된 토큰 입니다.", HttpStatus.UNAUTHORIZED),
+    EXPIRED_ACCESS_TOKEN("만료된 토큰 입니다.", HttpStatus.UNAUTHORIZED);
 
     private final String message;
     private final HttpStatus status;
--- src/main/java/com/hh/multiboarduserbackend/jwt/JwtProvider.java
+++ src/main/java/com/hh/multiboarduserbackend/jwt/JwtProvider.java
@@ -3,12 +3,14 @@ package com.hh.multiboarduserbackend.jwt;
 import com.hh.multiboarduserbackend.exception.CustomException;
 import com.hh.multiboarduserbackend.exception.JwtErrorCode;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.MacAlgorithm;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -17,6 +19,8 @@ import java.util.Date;
 
 @Component
 public class JwtProvider {
+
+    private static final long ACCESS_TOKEN_TIME = 1000 * 60 * 60 * 24 * 30 * 1000; // 한달
 
     private final JwtProperties jwtProperties;
     private final SecretKey key;
@@ -30,19 +34,21 @@ public class JwtProvider {
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
+
     /**
      * jwt 토큰 생성 메서드
      * @param memberId - auth value
      * @return - jwt token
      */
     public JwtToken generateToken(Long memberId) {
-        long now = (new Date()).getTime();
+
+        long now = (new Date(System.currentTimeMillis())).getTime();
 
         // Access Token 생성
-        Date exp = new Date(now + jwtProperties.expiration());
+        Date expiration = new Date(now + ACCESS_TOKEN_TIME);
         String accessToken = Jwts.builder()
                 .issuer(jwtProperties.issuer())
-                .expiration(exp)
+                .expiration(expiration)
                 .claim("auth", memberId)
                 .signWith(key, alg)
                 .compact();
@@ -64,7 +70,7 @@ public class JwtProvider {
         Claims claims = parseClaims(accessToken);
 
         if(claims.get("auth") == null) {
-            throw JwtErrorCode.REQUIRED_ACCESS_TOKEN.defaultException();
+            throw JwtErrorCode.NOT_AUTH_TOKEN_ERROR.defaultException();
         }
 
         return Long.parseLong(claims.get("auth").toString());
@@ -81,8 +87,10 @@ public class JwtProvider {
                     .verifyWith(key).build()
                     .parseSignedClaims(token);
             return true;
+        } catch (ExpiredJwtException e) {
+            throw JwtErrorCode.EXPIRED_ACCESS_TOKEN.defaultException(e);
         } catch (JwtException | IllegalArgumentException e) {
-            throw JwtErrorCode.REQUIRED_ACCESS_TOKEN.defaultException(e);
+            throw JwtErrorCode.WRONG_ACCESS_TOKEN.defaultException(e);
         }
     }
 
@@ -97,7 +105,7 @@ public class JwtProvider {
                     .verifyWith(key).build()
                     .parseSignedClaims(accessToken).getPayload();
         } catch (JwtException | IllegalArgumentException e) {
-            throw JwtErrorCode.REQUIRED_ACCESS_TOKEN.defaultException(e);
+            throw JwtErrorCode.RESOLVE_TOKEN_ERROR.defaultException(e);
         }
 
     }
